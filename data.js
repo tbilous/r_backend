e21dc98c2f3c8cf341ad3b52c878be80afff49ff
tbilous <tbilous@yahoo.com>
2017-06-22 11:59:10 +0300

    add posts

--- data.js
+++ data.js
@@ -29,6 +29,132 @@ module.exports = {
         updatedAt: '2017-06-01'
       },
       likes: 10,
+    },
+    {
+      id: 3,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla3',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 4,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla4',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 5,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla5',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 6,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla6',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 7,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla7',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 8,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla8',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 9,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla9',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 10,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla10',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
+    },
+    {
+      id: 11,
+      image: {
+        src: 'http://fakeimg.pl/300/FFFF00/',
+        alt: imgAttr.alt
+      },
+      text: 'Bla11',
+      signature: {
+        author: 'John Doe',
+        createdAt: '2017-06-01',
+        updatedAt: '2017-06-01'
+      },
+      likes: 10,
     }
   ]
 }
